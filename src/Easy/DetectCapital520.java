07185fa8c4255dc9b9dbcd36cbf3c97137dcf2ce
tonyliu0716 <tonyliu0716@gmail.com>
2017-05-10 23:54:01 -0500

    Add Detect Capital 520

---
+++ src/Easy/DetectCapital520.java
@@ -0,0 +1,30 @@
+package Easy;
+
+/**
+ * Created by tonyliu on 5/10/17.
+ *
+ * Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+ We define the usage of capitals in a word to be right when one of the following cases holds:
+
+ All letters in this word are capitals, like "USA".
+ All letters in this word are not capitals, like "leetcode".
+ Only the first letter in this word is capital if it has more than one letter, like "Google".
+ Otherwise, we define that this word doesn't use capitals in a right way.
+ Example 1:
+ Input: "USA"
+ Output: True
+ Example 2:
+ Input: "FlaG"
+ Output: False
+ *
+ */
+public class DetectCapital520 {
+
+    public boolean detectCapitalUse(String word) {
+        if (word.length() < 2) return true;
+        if (word.toUpperCase().equals(word)) return true;
+        if (word.substring(1).toLowerCase().equals(word.substring(1))) return true;
+        return false;
+    }
+}
