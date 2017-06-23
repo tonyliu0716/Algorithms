205b7a7e8f286a4c64b162251ac5af64df78deb3
tonyliu0716 <tonyliu0716@gmail.com>
2017-06-22 23:49:44 -0500

    Update Insertion Sort

--- src/ClassicAlgorithm/SelectionSort.java
+++ src/ClassicAlgorithm/SelectionSort.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
  *
  * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
  * 然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
- *   http://matt33.com/images/2016-03-26-sort/Selection_sort_animation.gif
+ *
  */
 public class SelectionSort {
 
