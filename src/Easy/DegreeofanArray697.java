e92597325272f9eb632ebf4da9d47ad5e4b9972d
tonyliu0716 <tonyliu0716@gmail.com>
2017-12-04 22:17:28 -0600

    Update Degree of an Array 697

--- src/Easy/DegreeofanArray697.java
+++ src/Easy/DegreeofanArray697.java
@@ -1,5 +1,7 @@
 package Easy;
 
+import java.util.*;
+
 /**
  * Created by tonyliu on 2017/12/4.
  *
@@ -28,7 +30,78 @@ public class DegreeofanArray697 {
 
 
 
+
         return 0;
+    }
+
+    public static int findSubArray(int[] nums) {
+
+        if(nums.length == 1) {
+            return nums.length;
+        }
+
+        HashMap<Integer, Integer> maps = new HashMap<>();
+
+        HashMap<Integer, Integer> maxApperMaps = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(maps.get(nums[i]) == null) {
+                maps.put(nums[i], 1);
+                maxApperMaps.put(nums[i], 1);
+            } else {
+                for(int j = 0; j < nums.length; j++) {
+                    if (nums[j] == nums[i]) {
+                        maps.put(nums[i], i - j + 1);
+                        break;
+                    }
+                }
+                maxApperMaps.put(nums[i], maxApperMaps.get(nums[i]) + 1);
+            }
+        }
+
+
+        int maxApperTime = 0;
+        ArrayList<Integer> lists = new ArrayList<>();
+        int maxApperNum = 0;
+        for(Map.Entry<Integer, Integer> entry : maxApperMaps.entrySet()) {
+            if(maxApperTime < entry.getValue()) {
+                maxApperTime = entry.getValue();
+            }
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        for(Map.Entry<Integer, Integer> entry : maxApperMaps.entrySet()) {
+            if(entry.getValue() ==  maxApperTime) {
+                lists.add(entry.getKey());
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < lists.size(); i++) {
+            System.out.println("Element of Lists: " + lists.get(i));
+            if(min > maps.get(lists.get(i))) {
+                min = maps.get(lists.get(i));
+            }
+        }
+
+        return min;
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        //int[] nums = {1,1,2,2,2,1};
+        //int[] nums = {1, 2, 2, 3, 1};
+        int[] nums = {1,2,2,3,1,4,2};
+//        int[] nums = {47,47,72,47,72,47,79,47,12,92,13,47,47,83,33,15,18,47,47,47,47,64,47,65,47,47,47,47,70,47,47,55,47,
+//                15,60,47,47,47,47,47,46,30,58,59,47,47,47,47,47,90,64,37,20,47,100,84,47,47,47,47,47,89,47,36,47,60,47,18,
+//                47,34,47,47,47,47,47,22,47,54,30,11,47,47,86,47,55,40,49,34,19,67,16,47,36,47,41,19,80,47,47,27};
+        System.out.println( DegreeofanArray697.findSubArray(nums));
+
+
     }
 
 }
