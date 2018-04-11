e2d3431b9954047ff295eed56b6f74f0535a0d8c
tonyliu0716 <tonyliu0716@gmail.com>
2018-04-11 18:02:39 -0500

    upload the matrix problem

---
+++ src/Easy/ToeplitzMatrix_766.java
@@ -0,0 +1,60 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by tao on 4/10/18.
+ *
+ * A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+ Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+
+ Example 1:
+
+ Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+ Output: True
+ Explanation:
+ 1234
+ 5123
+ 9512
+
+ In the above grid, the diagonals are "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]", and in each diagonal all elements are the same, so the answer is True.
+ Example 2:
+
+ Input: matrix = [[1,2],[2,2]]
+ Output: False
+ Explanation:
+ The diagonal "[1, 2]" has different elements.
+ Note:
+
+ matrix will be a 2D array of integers.
+ matrix will have a number of rows and columns in range [1, 20].
+ matrix[i][j] will be integers in range [0, 99].
+ */
+public class ToeplitzMatrix_766 {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int r = 0; r < matrix.length; ++r)
+            for (int c = 0; c < matrix[0].length; ++c)
+                if (r > 0 && c > 0 && matrix[r-1][c-1] != matrix[r][c])
+                    return false;
+        return true;
+    }
+
+    /* method to find the next element in the matrix is the same or not
+     1234
+     5123
+     9512
+     let's say start from 1, and find next 1 in the second array is the element you want or not
+     assume we start (0,0), which means the first element in the matrix
+     then (0,1), (0,2) ...
+    */
+
+
+    public static void main(String[] args) {
+
+    }
+}
