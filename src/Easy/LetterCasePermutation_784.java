c31d9c34d961ef0d9a48f915378ee81d65bdb2f3
tonyliu0716 <tonyliu0716@gmail.com>
2018-05-11 11:43:51 -0500

    upload the matrix problem

---
+++ src/Easy/LetterCasePermutation_784.java
@@ -0,0 +1,51 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tao on 4/11/18.
+ *
+ * Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  Return a list of all possible strings we could create.
+
+ Examples:
+ Input: S = "a1b2"
+ Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+
+ Input: S = "3z4"
+ Output: ["3z4", "3Z4"]
+
+ Input: S = "12345"
+ Output: ["12345"]
+ Note:
+
+ S will be a string with length at most 12.
+ S will consist only of letters or digits.
+ */
+
+// how about use set as a data structure
+public class LetterCasePermutation_784 {
+
+//    class TreeNote{
+//        Character c;
+//        TreeNote left;
+//        TreeNote right;
+//        TreeNote(Character c) {
+//            this.c = c;
+//        }
+//    }
+
+    public List<String> letterCasePermutation(String S) {
+        List<String> list = new ArrayList<>();
+
+        
+
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
