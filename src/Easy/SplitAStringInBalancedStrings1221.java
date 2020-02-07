6e8d62e3e714ae48f0838e7fbbff18c12fb5e0a4
tonyliu0716 <tonyliu0716@gmail.com>
2020-02-06 21:12:17 -0600

    Upload

--- src/Easy/SplitAStringInBalancedStrings1221.java
+++ src/Easy/SplitAStringInBalancedStrings1221.java
@@ -4,4 +4,39 @@ package Easy;
  * Created by tonyliu on 2020/2/6.
  */
 public class SplitAStringInBalancedStrings1221 {
+
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            i = SplitAStringInBalancedStrings1221.finder(i, s);
+            count++;
+        }
+        return count;
+    }
+
+    public static int finder(int index, String s) {
+        int numR = 0;
+        int numL = 0;
+        int returnIndex = 0;
+        for(int i = index; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') {
+                numR++;
+            }
+            if (s.charAt(i) == 'L') {
+                numL++;
+            }
+            if (numR == numL) {
+                returnIndex = i;
+                break;
+            }
+        }
+        return returnIndex;
+    }
+
+    public static void main(String[] args) {
+        SplitAStringInBalancedStrings1221 arr = new SplitAStringInBalancedStrings1221();
+        System.out.println(arr.balancedStringSplit("RLRRLLRLRL"));
+
+    }
+
 }
