3f61287e6c96fb75ac0d08eae58010871f991c88
TonyLiu <tonyliu@MacBook-Pro.hsd1.tx.comcast.net>
2020-10-19 11:20:39 -0500

    Added Generate parentheses 22.

---
+++ src/Medium/GenerateParentheses22.java
@@ -0,0 +1,58 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+*/
+public class GenerateParentheses22 {
+    // 4
+    // ()()()(), ()(()()), (())(()), ()((())), ((()))(),(()())()
+    public List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList<String>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current))
+                result.add(new String(current));
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos+1, result);
+            current[pos] = ')';
+            generateAll(current, pos+1, result);
+        }
+    }
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c: current) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return (balance == 0);
+    }
+
+    public static void main(String[] args) {
+        List<String> combinations = new GenerateParentheses22().generateParenthesis(3);
+        for(String s: combinations) {
+            System.out.println(s);
+        }
+    }
+
+}
